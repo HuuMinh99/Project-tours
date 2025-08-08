0130c44d883bcbda23cae967cc8a3690049ea9da
HuuMinh999 <minhhirosfi99@gmail.com>
2025-08-08 08:15:14 +0700

    delete

--- tour_project/Desktop/tour_project/booking/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BookingListCreateView, BookingDetailView
-
-urlpatterns = [
-    path('', BookingListCreateView.as_view(), name='booking-list-create'),
-    path('<int:pk>/', BookingDetailView.as_view(), name='booking-detail'),
-]
--- tour_project/Desktop/tour_project/tours/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TourListCreateView, TourRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('', TourListCreateView.as_view(), name='tour-list-create'),
-    path('<int:pk>/', TourRetrieveUpdateDestroyView.as_view(), name='tour-detail'),
-]
